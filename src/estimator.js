fb024eb15a89b9b5b0ce6b831a599bc7fe32f437
Hannas <hannasabdulwahab@gmail.com>
2020-04-12 16:04:57 -0700

    new challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,29 +1,28 @@
 const covid19ImpactEstimator = (data) => {
-    data = input;
-    const input = {
-        region:{
-            name: 'Africa',
-            avgAge: 19.7, 
-            avgDailyIncomeInUSD: 5, 
-            avgDailyIncomePopulation: 0.71
-        },
-        periodType: 'days',
-        timeToElapse: 58,
-        reportedCases: 674, 
-        population: 66622705, 
-        totalHospitalBeds: 1380614 
-    };
-const output = {
-    data: {},
+  const input = {
+    region: {
+      name: 'Africa',
+      avgAge: 19.7,
+      avgDailyIncomeInUSD: 5,
+      avgDailyIncomePopulation: 0.71
+    },
+    periodType: 'days',
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+  };
+  const output = {
+    data,
     impact: {},
     severeImpact: {}
-    };
-// Challenge 1
-const reportedCases = input.reportedCases;
-const factor = Math.floor(convertToDays(periodType, timeToElapse) / 3);
-output.impact.currentlyInfected = reportedCases * 10;
-output.severeImpact.currentlyInfected = reportedCases * 50;
-output.impact.infectionsByRequestedTime = output.impact.currentlyInfected * (2 ** factor);
-output.severeImpact.infectionsByRequestedTime = output.severeImpact.currentlyInfected * (2 ** factor);
+  };
+  // Challenge 1
+  //   const factor = Math.floor(convertToDays(periodType, timeToElapse) / 3);
+  const factor = Math.floor(input.periodType / 3);
+  output.impact.currentlyInfected = input.reportedCases * 10;
+  output.severeImpact.currentlyInfected = input.reportedCases * 50;
+  output.impact.infectionsByRequestedTime = output.impact.currentlyInfected * (2 ** factor);
+  output.severeImpact.infectionsByRequestedTime = output.severeImpact.currentlyInfected * (2 ** factor);
 };
 export default covid19ImpactEstimator;
