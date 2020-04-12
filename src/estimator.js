6677cba4ac30087ca497747a36aa9a4f928c8d59
Hannas <hannasabdulwahab@gmail.com>
2020-04-12 09:45:54 -0700

    challenge one

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,31 @@
 const covid19ImpactEstimator = (data) => {
-    const input = data;
-    return{
-        data: input,
-        impact: {},
-        severeImpact: {}
+    const input = {
+        region: {
+            name: "Africa",
+            avgAge: 19.7, 
+            avgDailyIncomeInUSD: 5, 
+            avgDailyIncomePopulation: 0.71
+        },
+        periodType: "days",
+        timeToElapse: 58,
+        reportedCases: 674, 
+        population: 66622705, 
+        totalHospitalBeds: 1380614 
     }
-};
+const output = {
+    data: {},
+    impact: {},
+    severeImpact: {}
+    };
+// Challenge 1
+const reportedCases = input.reportedCases;
+const factor = Math.floor(convertToDays(periodType, timeToElapse) / 3);
+output.impact.currentlyInfected = reportedCases * 10;
+output.severeImpact.currentlyInfected = reportedCases * 50;
+output.impact.infectionsByRequestedTime = output.impact.currentlyInfected * (2 ** factor);
+output.severeImpact.infectionsByRequestedTime = output.severeImpact.currentlyInfected *(2 ** factor);
+
+
+}
 
 export default covid19ImpactEstimator;
